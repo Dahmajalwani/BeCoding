f0280f23c8646c723f4b17e512873e5f974fa7b3
DAHMAJABER <dahma.alwani.jiz.s@tuwaiq.edu.sa>
2021-12-27 11:31:27 +0300

    init xcode project stracture

---
+++ BeCoding/BeCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeCoding
+//
+//  Created by dahma alwani on 23/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
